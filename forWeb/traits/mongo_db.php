f2c2d6073048345e3c9ffe6fb602feb64f880dfb
DoctorsPeppers <doctorspeppers@gmail.com>
2019-04-08 10:42:36 -0300

    Adding mongo_db trait

---
+++ forWeb/traits/mongo_db.php
@@ -0,0 +1,45 @@
+<?php
+namespace traits;
+
+
+trait database{
+
+
+    protected $host;
+    protected $user;
+    protected $password;
+    protected $connection;
+
+    public function setconnection($host, $user, $password, $db_name){
+        $this->mongo_host = $mongo_host;
+    }
+
+    protected function connect(){
+        $this->connection = new MongoClient($this->mongo_host);
+    }
+
+    protected function selectcollection($database,$collection){
+    	$this->connection = $this->connection->selectCollection($database,$collection);
+    }
+
+    protected function insertOne($array){
+    $result = $this->connection->insertOne([
+    'username' => 'admin',
+    'email' => 'admin@example.com',
+    'name' => 'Admin User',
+	]);
+    return $result;
+    }
+
+    protected function command($query){
+       $result = $this->connection->command($query);
+        
+       return $result;
+    }
+    
+    protected function closeConnection(){
+       $this->connection->close()
+    }
+
+        
+}
---
+++ forWeb/traits/template_db.php
@@ -0,0 +1,32 @@
+<?php
+namespace traits;
+
+
+trait database{
+
+
+    protected $host;
+    protected $user;
+    protected $password;
+    protected $connection;
+
+    public function setconnection($host, $user, $password, $dbname){
+
+    }
+
+    protected function connect(){
+        
+    }
+
+    protected function command($array,$query){
+        
+        
+        return $result;
+    }
+    
+    protected function closeConnection(){
+       
+    }
+
+        
+}
